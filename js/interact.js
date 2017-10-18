9f4de256b43af8483900ad174fe659051b6927c2
Erik Johnson <mindovermiles262@gmail.com>
2017-10-18 15:00:16 -0700

    display clicked button

--- js/interact.js
+++ js/interact.js
@@ -2,12 +2,15 @@
 // 2017-10-14 AD
 // Methods to interact with the buttons of the calculator
 
+let runningTotal = 0
 const calcButtons = document.getElementsByClassName("calcButton");
+const lcd = document.getElementsByClassName("lcd");
 
 // Gets value of calcButton div
 let value = function() {
   for (var i = 0; i < calcButtons.length; i++) {
     calcButtons[i].addEventListener('click', function() {
+      document.getElementsByClassName('lcd')[0].textContent = this.textContent;
       console.log(this.textContent);
     });
   }
